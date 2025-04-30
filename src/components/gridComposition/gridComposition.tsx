fef2f9becbf4c005312b89d2174542cf0a6bc8f6
pala <apalano23@gmail.com>
2025-04-30 17:52:20 +0200

    INFO: Scaffold of gridComposition

---
+++ src/components/gridComposition/gridComposition.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './gridComposition.scss';
+
+interface gridCompositionProps {}
+
+const gridComposition: FC<gridCompositionProps> = () => (
+  <div className="gridComposition">
+    gridComposition Component
+  </div>
+);
+
+export default gridComposition;
