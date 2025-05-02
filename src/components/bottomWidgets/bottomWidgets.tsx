95fa6a7a9c1f6fcc1152004e54d5ac4ff6fc7a7b
pala <apalano23@gmail.com>
2025-05-02 09:58:17 +0200

    FEAT: Added bottom widgets

--- src/components/bottomWidgets/bottomWidgets.tsx
+++ src/components/bottomWidgets/bottomWidgets.tsx
@@ -4,10 +4,10 @@ import timeService from "../../services/timeService";
 
 const WeatherGames = () => (
 
-    <div className={"wg-wrapper pb-5 d-flex justify-content-center flex-md-row flex-column me-2 ps-2 wg-border-bottom"}>
-        <div className={"col-md-6 col-12 d-flex flex-column wg-border-top me-auto"}>
+    <div className={"wg-wrapper pb-5 d-flex justify-content-center flex-md-row flex-column wg-border-bottom"}>
+        <div className={"col-md-6 col-12 d-flex flex-column wg-border-top me-3"}>
             <div className={"d-flex justify-content-between p-2 pt-2 mb-5 w-header"}>
-                <span className={"py-1"}>LOREM</span>
+                <span className={"py-1 fw-semibold"}>LOREM</span>
                 <span className={"py-1 px-3 w-top-button"}>LOREM IPSUM</span>
             </div>
             <div className={"d-flex w-row"}>
@@ -37,9 +37,9 @@ const WeatherGames = () => (
             </div>
         </div>
 
-        <div className={"col-md-6 col-12 d-flex flex-column wg-border-top ms-2 mt-5 mt-md-0"}>
+        <div className={"col-md-6 col-12 d-flex flex-column wg-border-top ms-3 mt-5 mt-md-0"}>
             <div className={"d-flex justify-content-between p-2 pt-2 mb-5 w-header"}>
-                <span className={"py-1"}>LOREM</span>
+                <span className={"py-1 fw-semibold"}>LOREM</span>
             </div>
             <div className={"d-flex w-row"}>
                 <div
@@ -58,10 +58,10 @@ const WeatherGames = () => (
     </div>
 );
 const Horoscope = () => (
-    <div className={"w-full d-flex flex-column align-items-start p-2 wg-border-bottom"}>
+    <div className={"w-full d-flex flex-column align-items-start p-2"}>
         <span className={"fw-semibold my-3"}>LOREM</span>
         <section
-            className={"mt-1 p-sm-3 w-100 d-flex overflow-x-scroll align-items-center justify-content-between gap-5 mb-4"}>
+            className={"mt-1 p-sm-3 w-100 d-flex overflow-x-scroll align-items-center justify-content-between gap-4 mb-4"}>
             {[...Array(12)].map((_, index) => (
                 <div className={"d-flex flex-column justify-content-center align-content-center"}>
                     <img alt={"horoscope sign"} className={"img-fluid"} src={"https://placehold.co/60x60"}/>
@@ -72,11 +72,56 @@ const Horoscope = () => (
     </div>
 )
 
+const WordsSection = () => (
+    <div className={"wg-wrapper pb-5 d-flex justify-content-center align-items-md-start align-items-center flex-md-row flex-column me-3 ps-2"}>
+        <div className={"col-md-6 col-12 d-flex flex-column me-2 wg-border-top"}>
+            <div className={"d-flex justify-content-between p-2 pt-2 mb-3 fs-6"}>
+                <span className={"py-1 fw-semibold"}>LOREM</span>
+            </div>
+            <div className={"w-full d-flex justify-content-center align-items-center w-font"}>
+                <span className={"wg-border-right pe-5 text-end"}>LOREM</span>
+                <span className={"ps-5 text-start"}>LOREM-IPSUM</span>
+            </div>
+            <label className={"w-full text-center w-font mt-3 px-4"}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</label>
+            <input type="text" name="cerca" className="wb-search" id="cerca" value=""/>
+            <div className={"d-flex align-content-center justify-content-center"}>
+                <button type="submit" name="vai" className="w-button" value="Cerca">CERCA</button>
+            </div>
+        </div>
+
+        <div className={"col-md-6 col-12 d-flex flex-column ms-3 wg-border-top wg-border-right wg-border-bottom wg-border-left"}>
+            <span className={"w-full text-center py-2 fs-5"}><span className={"text-danger"}>LO</span>REM<span className={"text-danger"}>IPSUM</span></span>
+            <div className={"overflow-x-hidden position-relative"}>
+                <img src={"https://placehold.co/800x176"} alt={"background-img"} className={"img-fluid"}/>
+                <div className={"position-absolute bottom-0 w-font ps-3"}>
+                    <span className={"text-white bg-danger p-1 w-auto"}>LOREM</span> <br />
+                    <span className={"fw-semibold text-white"}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam </span>
+                </div>
+            </div>
+            <div className={"d-flex ps-3 py-3"}>
+                <img alt={"book-cover"} src={'https://placehold.co/72x106'}/>
+                <div className={"ps-2 d-flex flex-column "}>
+                    <span className={"mb-2 fs-6"}>Lorem ipsum dolor</span>
+                    <span className={"w-font"}>LOREM IPSUM</span>
+                    <span className={"w-font text-danger"}> LO-REMIPSUM</span>
+                </div>
+            </div>
+            <div className={"wg-border-top w-100 w-font d-flex py-1"}>
+                <span className={"col-6 text-center wg-border-right"}>lorem ipsum dolor sit amet</span>
+                <span className={"col-6 text-center"}>lorem ipsum dolor sit amet</span>
+            </div>
+        </div>
+
+    </div>
+
+)
+
 export function BottomWidgets() {
     return (
         <div className="border-standard p-sm-4 py-3 px-1">
             <WeatherGames/>
             <Horoscope/>
+            <WordsSection/>
         </div>
     )
 }
