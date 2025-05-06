d7f2c915639d994e0a6ec7b1b43a361955b59e17
pala <apalano23@gmail.com>
2025-05-06 11:34:27 +0200

    INFO: Tweaked  margin and padding in Additional columns component

--- src/components/additionalColumns/additionalColumns.tsx
+++ src/components/additionalColumns/additionalColumns.tsx
@@ -4,15 +4,15 @@ import classNames from "classnames";
 
 export function additionalColumns({standard = false, sport = false, grey = false, hideSecondCol = false}) {
     return (
-        <div className={"w-full px-4"}>
+        <div className={"w-full px-sm-4 px-3"}>
             <div className={"add-cols-wrapper d-flex flex-column flex-md-row w-100 py-3"}>
                 <div
-                    className={classNames("col-12 d-flex flex-md-row flex-row-reverse justify-content-between justify-content-md-center ", hideSecondCol ? '' : 'col-md-8 adds-cols-div')}>
+                    className={classNames("col-12 d-flex flex-md-row flex-row-reverse justify-content-between justify-content-md-center ", hideSecondCol ? '' : 'col-md-8 adds-cols-div ps-3 ps-md-0')}>
                     <div className={classNames(hideSecondCol ? "col-4" : "col-6")}>
                         <img className={"img-fluid"} alt="additional info illustration"
                              src={"https://placehold.co/332x188"}/>
                     </div>
-                    <div className={classNames(hideSecondCol ? "col-8 ps-3" : "col-6")}>
+                    <div className={classNames(hideSecondCol ? "col-8 ps-sm-3" : "col-6")}>
                         <h2 className={"serif-font text-wrap fw-semibold h5 pe-2 text-start"}>
                             <span
                                 className={classNames({'text-orange': standard}, {"sport-title": sport}, {'color-text-gray': grey})}>Lorem</span>
